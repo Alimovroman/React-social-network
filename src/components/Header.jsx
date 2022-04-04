df904aa0c82dd502248e07448172a5d53aab8536
Roman <Alimov.ramon@gmail.com>
2022-04-04 18:43:12 +0300

    added an outline of the site

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className='header'>
+        <img src = "https://c0.klipartz.com/pngpicture/287/900/gratis-png-ilustracion-de-surfista-tabla-de-surf-surf-surf-el-mar-thumbnail.png" alt = 'logo'></img>
+      </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+const Profile = () => {
+  return (
+    <main className='main'>
+        <div>
+          <img src='https://www.gazetemistanbul.com/images/haberler/2015/09/quiksilver_iflas_bayragini_cekti_h12541_5805c.jpg' alt='banner'></img>
+        </div>
+        <div>
+          <img src='https://sun9-7.userapi.com/impf/c9232/v9232238/364c/CBvTxC4_8VA.jpg?size=130x130&quality=96&sign=75221a12a00bbe5d8d3b94bf4e5ca1fb&c_uniq_tag=_GmlDK_k-KhNVGKhUlfFLLur2XpCwEOrnVh6Mw16sfA&type=album' alt='avatar'></img>
+          + description
+        </div>
+        <div>
+          my post
+          <div>
+            new post
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
+      </main>
+  )
+};
+
+export default Profile;
